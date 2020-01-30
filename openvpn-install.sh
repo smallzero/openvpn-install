ddf57dfffc6b26a47e9b46a21d37cbf04a8c184c
Agus Rochmad Tri Raharjo <abumamail@gmail.com>
2020-01-30 15:04:11 +0700

    last update at Thu, Jan 30, 2020  3:04:11 PM

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -993,6 +993,8 @@ function newClientOneLiner(){
 	./easyrsa build-client-full "$CLIENT" nopass
 	#useradd  -p $(openssl passwd -1 $CLIENT_PASSWD) $CLIENT
 	useradd -m $CLIENT
+	echo "${CLIENT}:${CLIENT_PASSWD}"
+	sleep 3
 	echo "${CLIENT}:${CLIENT_PASSWD}" | chpasswd
 	# Home directory of the user, where the client configuration (.ovpn) will be written
 	if [ -e "/home/$CLIENT" ]; then  # if $1 is a user name
